c10492a88d9c65874c8b896b16c07ab346f9a2ec
ZornitsaSD <dzorunitsa@yahoo.fr>
2016-01-25 21:18:03 +0200

    Zadacha

---
+++ Zadacha.php
@@ -0,0 +1,16 @@
+<?php
+//a e promenliva
+$a = 15;
+$b = $a*5;
+$s = $a*$a;
+echo "Area is =" .$s . "sm2";
+
+$a = 10;
+$b = 15;
+$c = 25;
+
+$res = ($a + $b)/($c*2)."-";
+echo $res;
+
+$res1 = ($a + $b)/$c*2;
+echo $res1;
