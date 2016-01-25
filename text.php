6e432b5a37b92f2dafc5f1f9dfff287e4ac5e6ba
ZornitsaSD <dzorunitsa@yahoo.fr>
2016-01-25 21:18:23 +0200

    text

---
+++ text.php
@@ -0,0 +1,13 @@
+<?php
+//comment
+//comment
+/*comment.......
+*/
+//first values
+
+$role = "student";
+$course = "name of the course";
+$duration = 4;
+$user = "Dimitar";
+$text = "Hi, user! You've been approved to take part in the $course 1 as a $role. The $course 1 will last for $duration days.";
+echo $text;
